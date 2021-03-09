83670da3effccc86f6ff87a043cd694a406efc58
CorneliaRoehrer <corne.lia.roehrer@gmail.com>
2021-03-09 11:57:54 +0100

    add basic ex 2

--- basic_exercise_1/script.js
+++ basic_exercise_1/script.js
@@ -1,3 +1,5 @@
+// basic exercise 1
+
 function output () {
     var firstName = document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
@@ -10,6 +12,40 @@ function output () {
     }
 
     document.getElementById("outputLine").innerHTML = "First Name: " + firstName + "<br>Last Name: " + lastName + "<br>Age: " + age;
+
+    var profession = document.getElementById("select").value;
+    console.log(profession);
+
+    switch (profession){
+        case "IT":
+            document.getElementById("outputLine").style.backgroundColor = "purple";
+            break;
+        case "Hospitality":
+            document.getElementById("outputLine").style.backgroundColor = "yellow";
+            break;
+        case "Clown":
+            document.getElementById("outputLine").style.backgroundColor = "brown";
+            break;
+        case "Teacher":
+            document.getElementById("outputLine").style.backgroundColor = "pink";
+            break;
+        case "Doctor":
+            document.getElementById("outputLine").style.backgroundColor = "orange";
+            break;
+    }
 };
 
 document.getElementById("submitBtn").addEventListener("click",output);
+
+// basic exercise 2
+
+
+
+// function result(){
+//     var select = document.getElementById("select").value;
+//     console.log(check);
+//     console.log(select);
+//     // console.log(value)
+//     document.getElementById("result").innerHTML = value;
+// }
+// document.getElementById("btn").addEventListener("click",result);
